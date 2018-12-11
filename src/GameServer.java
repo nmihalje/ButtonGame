46c3acb441f3c33689f67f43feba846e00bdb5e1
nmihalje <nmihalje@htl-steyr.ac.at>
2018-12-11 08:45:11 +0100

    first commit

--- src/GameServer.java
+++ src/GameServer.java
@@ -3,11 +3,16 @@ import java.awt.event.ActionListener;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameServer {
 	private int port;
 	private boolean running = false;
 	private boolean clientready = false;
+
+	Random random_waittime = new Random();
+	Random random_buttons = new Random();
+	int[] randombuttonarray;
 
 	private ArrayList<GamePlayer> clients = new ArrayList<>();
 
@@ -18,9 +23,20 @@ public class GameServer {
 	private ActionListener broadcastListener = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			if (e.getActionCommand().equals("ready")) {
-
+				
+				int randomTime = random_waittime.nextInt(4000) + 2000;
+				
+				for (int j = 0; j <= 3; j++) {
+					int randombutton = random_waittime.nextInt(1) + 15;
+					randombuttonarray[j] = randombutton;
+				}
 				for (GamePlayer gp : clients) {
-					//TODO: 
+
+					gp.send("" + randomTime);
+
+					gp.send("" + randombuttonarray[0] + "," + randombuttonarray[1] + "," + randombuttonarray[2] + ","
+							+ randombuttonarray[3]);
+					System.out.println("" + randombuttonarray[0]);
 				}
 			} else if (e.getActionCommand().equals("finished")) {
 				// sende Nachricht an alle clients
@@ -28,7 +44,6 @@ public class GameServer {
 				for (GamePlayer gp : clients) {
 					if (!gp.equals(e.getSource())) {
 						gp.send("you lose");
-						
 					}
 				}
 
